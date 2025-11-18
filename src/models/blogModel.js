ca481f0ec5fd1a9c962a58d7939000afc80a25ee
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-18 23:45:43 +0300

    feat: add mongoose schema with validation rules

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -4,12 +4,31 @@
 ------------------------------------------------------- */
 // Mongoose:
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+// const ModelName = new mongoose.Schema({ ...fields }, { ...settings })
+const ModelName = new mongoose.Schema(
+  {
+    // PrimaryKey (_id) tanımlamaya gerek yoktur. Otomatik tanımlanır.
+    // _id: Number
+
+    fieldName: {
+      type: Number,
+      default: null, // Veri gelmediğinde yazılacak veri.
+      trim: true, // Başındaki/sonundaki boşlukları kırpar.
+      unique: true, // Benzersiz kayıt.
+      index: true, // Aramalarda hızlı erişim sağlar.
+      // required: true, // Veri gönderimi zorunlu mu?
+      required: [true, "Bu data mutlaka gönderilmelidir."], // [Veri gönderimi zorunlu mu?, HATA MESAJI]
+      // enum: ['1', '2', '3']
+      // enum: [1, 2, 3] // Belirli değerlerden biri olmak zorunda.
+      enum: [[1, 2, 3], "Bu değerlerden biri olmalıdır."], // Belirli değerlerden biri olmak zorunda.
+    },
+  },
+  { ...settings }
+);
 
 /* ------------------------------------------------------- */
 // BlogCategory Model:
-
-
-
 
 /* ------------------------------------------------------- */
