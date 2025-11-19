aa0487677d0c4180653abf329a0d71599236ed98
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-19 15:49:38 +0300

    feat(models): add BlogPostSchema and export BlogCategory & BlogPost models

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -66,4 +66,54 @@ const BlogCategorySchema = new mongoose.Schema(
   }
 );
 
+// Set Model:
+// const BlogCategory = mongoose.model('BlogCategory', BlogCategorySchema)
+
 /* ------------------------------------------------------- */
+// BlogPost Schema:
+
+const BlogPostSchema = new mongoose.Schema({
+
+    // _id
+
+    categoryId: { // Default Relation: ManyToOne
+        type: mongoose.Schema.Types.ObjectId, // Hexadecimal Format: 'fedcba9876543210'
+        ref: 'BlogCategory', // ID hangi Model'e ait. (mongoose.model('ModelName', Schema))
+        required: true,
+        // unique: true, // Convert to OneToOne Relation.
+    },
+     title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    content: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    // createdAt // timestamps: true
+    // updatedAt // timestamps: true
+
+}, {
+    collection: 'blogPosts',
+    timestamps: true
+})
+
+// Set Model:
+// const BlogPost = mongoose.model('BlogPost', BlogPostSchema)
+
+/* ------------------------------------------------------- */
+
+// module.exports = {
+//     BlogCategory: BlogCategory,
+//     BlogPost: BlogPost
+// }
+
+/* ------------------------------------------------------- */
+module.exports = {
+    BlogCategory: mongoose.model('BlogCategory', BlogCategorySchema),
+    BlogPost: mongoose.model('BlogPost', BlogPostSchema)
+}
