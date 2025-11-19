540b448d3834f82b0016271ef10170942560a5c6
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-19 15:19:46 +0300

    feat(schema): add new field validations and options

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,31 +1,46 @@
 "use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
-------------------------------------------------------- */
+------------------------------------------------------- *
 // Mongoose:
 
 const mongoose = require("mongoose");
 
 // const ModelName = new mongoose.Schema({ ...fields }, { ...settings })
-const ModelName = new mongoose.Schema(
-  {
-    // PrimaryKey (_id) tanımlamaya gerek yoktur. Otomatik tanımlanır.
-    // _id: Number
+const ModelSchema = new mongoose.Schema(
+    {
 
-    fieldName: {
-      type: Number,
-      default: null, // Veri gelmediğinde yazılacak veri.
-      trim: true, // Başındaki/sonundaki boşlukları kırpar.
-      unique: true, // Benzersiz kayıt.
-      index: true, // Aramalarda hızlı erişim sağlar.
-      // required: true, // Veri gönderimi zorunlu mu?
-      required: [true, "Bu data mutlaka gönderilmelidir."], // [Veri gönderimi zorunlu mu?, HATA MESAJI]
-      // enum: ['1', '2', '3']
-      // enum: [1, 2, 3] // Belirli değerlerden biri olmak zorunda.
-      enum: [[1, 2, 3], "Bu değerlerden biri olmalıdır."], // Belirli değerlerden biri olmak zorunda.
+        // PrimaryKey (_id) tanımlamaya gerek yoktur. Otomatik tanımlanır.
+        // _id: Number
+
+        fieldName: {
+            type: Number, // JS Veri tipi.
+            default: null, // Veri gelmediğinde yazılacak veri.
+            trim: true, // Başındaki/sonundaki boşlukları kırpar. (String)
+            unique: true, // Benzersiz kayıt.
+            index: true, // Aramalarda hızlı erişim sağlar.
+            // required: true, // Veri gönderimi zorunlu mu?
+            required: [true, 'Bu data mutlaka gönderilmelidir.'], // [Veri gönderimi zorunlu mu?, HATA MESAJI]
+            // enum: ['1', '2', '3'],
+            // enum: [1, 2, 3], // Belirli değerlerden biri olmak zorunda.
+            enum: [[1, 2, 3], 'Bu değerlerden biri olmalıdır.'], // Belirli değerlerden biri olmak zorunda.
+            // validate: (data) => true, // Gelen data formatının doğrulunu kontrol etme.
+            validate: [
+                (data) => { return true },
+                'GÖnderilen data formatı yanlıştır.'
+            ], // Gelen data formatının doğrulunu kontrol etme.
+            get: (data) => data, // Bu field'a erişilmek istendiğinde otomatik çalışan fonksion.
+            set: (data) => data, // Bu field'a veri kaydedilmek istendiğinde otomatik çalışan fonksion.
+        }
+
+
+    }, {
+        collection: 'tableName', // Tablo İsmi
+        timestamps: true // createdAt ve updatedAt otomatik yönetilsin. (tanımlamaya gerek yok)
     },
-  },
-  { ...settings }
+)
+    
+  const ModelName = mongoose.model('ModelName', ModelSchema)
 );
 
 /* ------------------------------------------------------- */
