0821697c104b7d2ab0242ce136c3ff5f7677e3c9
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-20 15:53:45 +0300

    feat: add initial blog router with base route structure

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -2,3 +2,20 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const router = require('express').Router()
+
+// Call Controllers:
+const { blogCategory, blogPost } = require('../controllers/blogController')
+
+
+/* ------------------------------------------------------- */
+
+// URL: /blog ->
+
+// BlogCategory
+router.route('/')
+    .post()
+
+/* ------------------------------------------------------- */
+module.exports = router
