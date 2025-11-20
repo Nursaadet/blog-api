a6f502ca59ebbfa876e16660dc21d1cd4cf19d51
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-20 15:52:38 +0300

    feat: add initial BlogCategory and BlogPost controller structure

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,3 +2,27 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// Call Models:
+const { BlogCategory, BlogPost } = require('../models/blogModel')
+
+/* ------------------------------------------------------- */
+// BlogCategory Controller:
+
+module.exports.blogCategory = {
+
+    create: async (req, res) => {
+
+        
+
+    }
+
+}
+
+/* ------------------------------------------------------- */
+// BlogPost Controller:
+
+module.exports.blogPost = {
+    
+}
+
+/* ------------------------------------------------------- */
