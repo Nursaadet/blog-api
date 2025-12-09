875ad9c2a7ccdf7183708a2273e6ecc03a3419fd
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-09 13:01:32 +0300

    feat: implement User module - Add User schema and model - Set up object-based model export - Create User controller with full CRUD operations

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,3 +6,35 @@
 const mongoose = require('mongoose')
 
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    firstName: String,
+
+    lastName: String,
+
+}, {
+
+    collection: 'users',
+    timestamps: true
+
+})
+
+/* ------------------------------------------------------- */
+
+// module.exports = mongoose.model('User', UserSchema) // Direct
+module.exports.User = mongoose.model('User', UserSchema) // In Object
+
