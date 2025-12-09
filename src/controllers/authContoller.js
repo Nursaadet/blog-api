b81ddfa16e6f7bd823bd2b81fc0a068e4430ba1f
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-09 16:24:18 +0300

    feat: add passwordEncrypt helper and implement email/password validation in login

--- src/controllers/authContoller.js
+++ src/controllers/authContoller.js
@@ -9,7 +9,21 @@ const { User } = require('../models/userModel')
 module.exports.auth = {
 
     login: async (req, res) => {
+const { email, password } = req.body
 
+        if (email && password) {
+
+            // const user = await User.findOne({ email: email })
+            const user = await User.findOne({ email })
+
+            if (user) {
+
+            }
+
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Email and password are required.')
+        }
     },
 
     logout: async (req, res) => {
