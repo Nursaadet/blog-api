7c70bf650c06edf2a46f59a7b6d1e91576c72fbb
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-09 23:25:16 +0300

    Add sync.js with project header and model imports

---
+++ sync.js
@@ -0,0 +1,10 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const { User } = require('./src/models/userModel')
+const { BlogCategory, BlogPost } = require('./src/models/blogModel')
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
