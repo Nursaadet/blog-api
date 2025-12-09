625fcf4bb46039f4c02f63ffc59e983f33273485
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-09 18:05:52 +0300

    Add user session validation and set req.user

--- src/middlewares/userControl.js
+++ src/middlewares/userControl.js
@@ -4,4 +4,8 @@
 ------------------------------------------------------- */
 // Authentication Middleware
 // Session içindeki user id ve passwordu kontrol eden middleware.
+module.exports = async (req, res, next) => {
+    next()
+
+}
 /* ------------------------------------------------------- */
