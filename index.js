c3c3cae438be28fc6d7ee214378db7b0ea0c5fc0
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-09 16:47:56 +0300

    feat: add root route with session info

--- index.js
+++ index.js
@@ -11,44 +11,50 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 // Accept JSON:
-app.use(express.json())
+app.use(express.json());
 
 // DB Connection:
 // const dbConnection = require("./src/dbConnection");
 // dbConnection();
-require('./src/dbConnection')()
+require("./src/dbConnection")();
 
 // Catch error from async:
-require('express-async-errors')
+require("express-async-errors");
 /* ------------------------------------------------------- */
 // SessionCookies:
 // http://expressjs.com/en/resources/middleware/cookie-session.html
 // https://www.npmjs.com/package/cookie-session
 //* $ npm i cookie-session
 
-const session = require('cookie-session') // Session Middleware
+const session = require("cookie-session"); // Session Middleware
 
-app.use(session({  // General Settings.
+app.use(
+  session({
+    // General Settings.
     secret: process.env.SECRET_KEY, // Cookie datası şifreleme anahtarı
     // maxAge: 1000 * 60 * 60 * 24 * 3 // miliSeconds // 3 Days
-}))
+  })
+);
 /* ------------------------------------------------------- */
 
-app.all('/', (req, res) => {
-    res.send('WELCOME TO BLOG API')
-})
+app.all("/", (req, res) => {
+  res.send({
+    session: req.session,
+    message: "WELCOME TO BLOG API",
+  });
+});
 
 /* ------------------------------------------------------- */
 // Routes:
-app.use('/auth', require('./src/routes/authRouter')) // User Model - Login/Logout
-app.use('/user', require('./src/routes/userRouter')) // User Model
-app.use('/blog', require('./src/routes/blogRouter')) // BlogCategory & BlogPost
+app.use("/auth", require("./src/routes/authRouter")); // User Model - Login/Logout
+app.use("/user", require("./src/routes/userRouter")); // User Model
+app.use("/blog", require("./src/routes/blogRouter")); // BlogCategory & BlogPost
 
 /* ------------------------------------------------------- */
 
 // Catch Errors:
-app.use(require('./src/errorHandler'))
+app.use(require("./src/errorHandler"));
 
 /* ------------------------------------------------------- */
 
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
